b04ee41a6d777761d2b0cbef897dd3a753ec6167
Robin Schellius <r.schellius@avans.nl>
2017-04-19 13:55:08 +0200

    opgeschoond

--- les-1/MyFirstServer/server-4-json-types.js
+++ les-1/MyFirstServer/server-4-json-types.js
@@ -2,19 +2,19 @@ var http = require('http');
 
 http.createServer(function(request, response){
 	console.log('Er was een request.');
-	response.writeHead(200, {'Content-Type': 'text/json'});
+	response.writeHead(200, {'Content-Type': 'application/json'});
 
 	var exampleArray = ["item1", "item2"];
 	var exampleObject = { 
 		item1: "item1val", 
 		item2: "item2val" 
 	};
-	var json = JSON.stringify({ 
+	var example = JSON.stringify({ 
 		anObject: exampleObject, 
 		anArray: exampleArray, 
 		another: "item"
 	});
-	response.end(json);
+	response.end(example);
 }).listen(3000);
 
 console.log('De server luistert op port 3000');
