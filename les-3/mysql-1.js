51df73580a5a76bd0a128fb07f2fae71fb5c07f8
Robin Schellius <r.schellius@avans.nl>
2017-05-19 11:45:21 +0200

    update na les 4

--- les-3/mysql-1.js
+++ les-3/mysql-1.js
@@ -1,20 +1,21 @@
 var mysql = require('mysql');
 
 var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'node_mysql_user',
-  password : process.env.DB_PASSWORD,
-  database : 'sakila'
+	host     : 'localhost',
+	user     : 'node_mysql_user',
+	password : process.env.DB_PASSWORD,
+	database : 'sakila'
 });
 
 connection.connect();
 
-connection.query('SELECT * from actors LIMIT 3', function(error, rows, fields) {
-  if (error)
-    console.log('' + error);
-  else
-    console.dir(rows);
-});
+connection.query('SELECT * from actors LIMIT 3', 
+	function(error, rows, fields) {
+		if (error)
+			console.log('' + error);
+		else
+			console.dir(rows);
+		});
 
 connection.end();
 
